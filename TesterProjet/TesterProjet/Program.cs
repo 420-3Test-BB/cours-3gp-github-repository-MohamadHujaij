22c46cc77ec915363c7e610b53ba5cee1924384d
Mohamad Hujaij <mohamad_01@outlook.com>
2020-10-06 16:39:30 -0400

    premiere modif

---
+++ TesterProjet/TesterProjet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TesterProjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("allo");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
